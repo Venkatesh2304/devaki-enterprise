bae8e168387025eeff93b6f6710d7f2ebf5a8ab8
Venkatesh2304 <96430022+Venkatesh2304@users.noreply.github.com>
2022-11-28 12:42:36 +0530

    completed the request version of devaki enterprise (home) v.1

---
+++ hul.py
@@ -0,0 +1,257 @@
+import imp
+from urllib import response
+from requests import Session
+from datetime import datetime
+import json
+from collections import defaultdict
+import curlify
+import pandas as pd
+from flask import jsonify, send_file, make_response
+import ewaysite
+import einvsite
+from io import BytesIO
+import json_converter
+import outstanding
+import openpyxl
+from itertools import combinations
+
+REPORT_URL = "/rsunify/app/reportsController/generatereport.do"
+AJAX_FILE = r"ajax.txt"
+
+
+# small functions
+def date(): return int((datetime.now() - datetime(1970, 1, 1)
+         ).total_seconds() * 1000) - (330*60*1000)
+
+
+class ikea(Session):
+    def __init__(self, user, db, isReload = True ):
+        super().__init__()
+        self.db = db
+        self.user = user
+        with open(AJAX_FILE) as f:
+          self.ajax_template = eval(f.read())
+        if isReload : 
+            self.reload()
+        self.json = {'Content-type': "application/json; charset=utf-8"}
+        self.download = lambda url: self.get(
+            "/rsunify/app/reportsController/downloadReport?filePath="+url)
+
+    def post(self, url, **kwargs):
+        url = self.baseUrl + url if self.baseUrl not in url else url 
+        kwargs["url"] = url
+        res = super().post(**kwargs)
+        # print(curlify.to_curl(res.request))
+        #print(res.text)
+        if res.status_code == 200:
+            return res
+        else:
+            self.login()
+            print("retry")
+            self.post(**kwargs)
+
+    def ajax(self, key, replaces):
+        temp = {}
+        for key, value in self.ajax_template[key].items():
+            if type(value) == str:
+               for orig, repl in replaces.items():
+                   value = value.replace("_"+orig+"_", repl)
+               temp[key] = value
+        return temp
+
+    def get(self, url, **kwargs):
+        url = self.baseUrl + url
+        res = super().get(url, **kwargs)
+        if res.status_code == 200:
+            return res
+        else:
+            self.login()
+            self.get()
+
+    def reload(self):
+        self.cookies.clear()
+        session = self.db.find_one({"username": self.user})
+        if session and "session" in session.keys():
+           cookies = json.loads(session["session"])
+           for key, value in cookies.items():
+               self.cookies.set(key, value)
+           for attr in ["baseUrl", "ikea_user", "ikea_pwd", "dbName"]:
+             self.__setattr__(attr, session[attr])
+        else:
+            self.login()
+
+    def login(self):
+        print("login started")
+        self.cookies.clear()
+        data = self.db.find_one({"username": self.user})
+        for attr in ["baseUrl", "ikea_user", "ikea_pwd", "dbName"]:
+            self.__setattr__(attr, data[attr])
+
+        data = {'userId': self.ikea_user, 'password': self.ikea_pwd,
+            'dbName': self.dbName, 'datetime': date(), 'diff': -330}
+        res = self.post('/rsunify/app/user/authentication.do',
+                  data=data).text   # need to handle errors
+        if "<body>" in res : 
+             return False , "Login Credentials is Wrong ."
+        if "Invalid" in res : 
+            return False , res 
+        if res == "CLOUD_LOGIN_PASSWORD_EXPIRED" : 
+           self.post("/rsunify/app/masterPasswordController/lemonPasswordReset", data = {
+            "newpassword": self.ikea_pwd , "confirmpassword": self.ikea_pwd }) 
+           return self.login() 
+
+
+        res = self.post("/rsunify/app/user/authenSuccess.htm")
+        if res.status_code != 200 :
+            return False 
+        # store the session cookies in db
+        self.db.update_one({"username": self.user}, {
+                           "$set": {"session": json.dumps(dict(self.cookies))}})
+        return True , "Successfully Login"
+
+    def getBeats(self, fromDate, toDate):
+        data = {'jasonParam': '{}',
+            'procedure': 'Beat_Selection_Procedure', 'orderBy': '[MKM_NAME]'}
+        data['jsonObjWhereClause'] = '{":P1": "' + fromDate.strftime(
+            "%Y%m%d") + '",":P2": "' + toDate.strftime("%Y%m%d") + '",":P3":"Both",":P4":"SecBills"}'
+        res = self.post(
+            "/rsunify/app/reportsController/getReportScreenDatawithprocedure.do", data=data)
+        # beats is returned in text format [[1,'AKBAR'],[4,'THIRU']]
+       
+        beats = json.loads(res.text)
+
+        clean = lambda x:   max(x.replace(" ", "-").replace("+", "-").split("-"),key=len)
+        beats = list(map(lambda x: [ x[0], clean(x[1]) ], beats))
+        #additional cleaning : 
+        isSame = lambda s1 , s2 :   (len( list(set(s1) ^ set(s2)) ) <= 1 ) or  s1 in s2 or s2 in s1 
+        
+        temp = defaultdict(list)
+        for beatId, beatName in beats:
+            for alreadyBeat in temp.keys() :  
+               if isSame(alreadyBeat,beatName) : 
+                 temp[alreadyBeat].append(beatId)
+                 break 
+            else : 
+                temp[beatName].append(beatId)
+
+        beats = dict((key, tuple(val)) for key, val in temp.items())
+        print( beats )
+        return beats
+
+    def Edownload(self, type, fromDate, toDate, data, beats, vehicles):
+        fromDate, toDate = fromDate.strftime(
+            "%Y%m%d"), toDate.strftime("%Y%m%d")
+        billwise_data = {}
+        excel_data = []
+        for key, value in data.items():  # key is beat or billno , value is vehicle name
+            if "-" in key:  # bills
+                [fromBill, toBill] = key.split("-")
+                data = self.ajax(type + "_download", {"fromDate": fromDate, "toDate": toDate,
+                          "beats": "", "fromBill": fromBill, "toBill": toBill})
+            else:  # beats
+                data = self.ajax(type + "_download", {"fromDate": fromDate, "toDate": toDate,
+                          "beats": ",".join([str(i) for i in beats[key]]), "fromBill": "", "toBill": ""})
+            res = self.post(REPORT_URL, data=data)
+
+            if res.text.strip() == "":
+                continue  # nothing to download
+            binary_content = self.download(res.text).content
+
+            excel = pd.read_excel(BytesIO(binary_content))
+            # if type == "eway" :  #eway bill transporter details
+            excel["Trans Name"] = value["vehicle"]
+            excel["Vehicle No"], excel["Distance level(Km)"] = value["vehicle"], value["distance"]
+            excel_data.append(excel)
+
+        if len(excel_data) == 0:
+              return False
+        return pd.concat(excel_data)  # the final dataframe
+
+    def EGenerate(self, types, fromDate, toDate, data, beats, vehicles):
+         maps = {"eway":  {"esession": ewaysite.Eway, "json":  json_converter.ewayJson},
+           "einvoice":  {"esession": einvsite.Einvoice, "json": lambda data: json_converter.einvJson(data, isVeh=True)}}
+
+         esession = maps[types]["esession"]()
+         if not esession.reload(self.user, self.db):  # reloading failed
+             return jsonify({"err": "Relogin Again"}), 520
+
+         data = self.Edownload(types, fromDate, toDate, data, beats, vehicles)
+         if type(data) == bool and data == False:
+            return jsonify({"err": "No Data Found"}), 521
+
+         json_data = maps[types]["json"](data)
+         return jsonify(esession.upload(json_data))
+
+    def outstanding(self, date=None , days = 20):
+        salesman = self.post("/rsunify/app/paginationController/getPopScreenData", data= json.dumps({
+            "jasonParam": { "viewName":"VIEW_LOAD_SALESMAN_BEAT_LINK_SALESMAN_LIST"}
+        }) , headers = self.json ).json() 
+        sal_id = map( lambda x : x[1] , salesman[0][1:])
+        beats_data = []
+        day = date.strftime('%A').lower() + "Linked"
+
+        for id in sal_id : 
+             beats_data += self.post("/rsunify/app/salesmanBeatLink/getSalesmanBeatLinkMappings",
+                      data={"divisionId": 0, "salesmanId": int(id) }).json()
+        beats_data = pd.DataFrame(beats_data)
+        #print(beats_data.columns)
+        beats_data.to_excel("exc.xlsx")
+        filteredBeats = list(set(beats_data[beats_data[day] != '0']["beatId"]))
+        
+       
+        data =  self.ajax("outstanding_download", {"date" : date.strftime("%Y-%m-%d") , "beats": ",".join(filteredBeats)})
+    
+
+        res = self.post("/rsunify/app/reportsController/generatereport.do" , data = data )
+        binary_content = self.download(res.text).content
+        excel = pd.read_excel(BytesIO(binary_content))
+    
+        return send_file(outstanding.interpret(excel,days) , as_attachment=True , download_name="outstanding.xlsx")
+
+    def creditlock(self , config ) :
+        print(config.find_one({"username" : self.user}))
+        config = config.find_one({"username" : self.user})["creditlock"] 
+        default = config["OTHERS"]
+        del config["OTHERS"]
+        config = dict(sorted(config.items(), key=lambda item:  item[1] if item[1] != 0 else 10000 , reverse=True))
+        
+        partyMaster = pd.read_excel("/home/venkatesh/Downloads/party.xlsx" , skiprows = 9)
+        partyMaster["PAR CODE HLL"] = partyMaster["HUL Code"]
+
+
+        url =self.post("/rsunify/app/reportsController/generatereport.do" , data = self.ajax("creditlock_download",{})).text 
+        creditlock_binary = BytesIO(self.download(url).content)
+        
+        creditlock = pd.read_excel(creditlock_binary)
+        
+        def beatType(beat) : 
+            for type , max_bills in config.items() : 
+                if type in beat : 
+                   return max_bills
+            return default 
+            
+        partyMaster["max_bills"] = partyMaster["Beat"].apply(beatType)
+        partyMaster =  partyMaster.drop_duplicates(subset=['PAR CODE HLL'], keep='first')[["PAR CODE HLL","max_bills"]]
+
+        creditlock = pd.merge(creditlock , partyMaster , on = "PAR CODE HLL",how="left")
+        
+        max_finder = lambda row : max(row["PAR CR BILLS UTILISED"],row["max_bills"]) if row["max_bills"] != 0 else 0
+        creditlock["max_bills"] = creditlock.apply( max_finder , axis = 1 )
+        creditlock = creditlock[creditlock["max_bills"] != creditlock['PAR CR BILLS']]
+        creditlock_binary.seek(0)
+        
+        
+        wb = openpyxl.load_workbook(creditlock_binary)
+        ws = wb['Credit Locking']
+        col = 6 
+        for idx , row in creditlock.iterrows() : 
+            ws.cell( int(row["Sr No"]) + 1 , 6).value = row["max_bills"]
+        
+        x = BytesIO()
+        wb.save(x)
+        x.seek(0)
+
+        files = { "file" : ("credit.xlsx", x ,'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')  }
+        res = self.post("/rsunify/app/beatSequenceMaster/uploadFileCLSU", files = files ).text 
+        return  jsonify({ "count" : len(creditlock.index)  , "res" : res }) , 200   
+
--- index.py
+++ index.py
@@ -1,93 +1,232 @@
-from pickle import TRUE
-from flask import *
-import pandas as pd 
-from selenium import webdriver 
-from datetime import datetime 
+from fileinput import filename
+import hashlib
 from datetime import timedelta
-import outstanding 
-import creditlock 
-from eway.EGenerate import EGenerate
-import eway.esync as esync
-import os 
-import shutil
-import chromedriver_autoinstaller
-import webbrowser
+from urllib import response
+# Flask, request, jsonify , send_file , make_response , Response , send_from_directory
+from flask import *
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity, set_access_cookies
+from pymongo import MongoClient
+from hul import *
+import ewaysite
+from flask_cors import CORS
+# date parser
+def dateParser(date): return datetime.strptime(date, "%Y-%m-%d")
+
+def SendExcel(df,download_name):
+     output = BytesIO()
+     with pd.ExcelWriter(output, engine='xlsxwriter') as writer : 
+         df.to_excel(writer,index=False)
+         writer.save()
+     output.seek(0)
+     return  send_file( output ,  mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" , 
+                    download_name= download_name )
 
 
 app = Flask(__name__)
-app.config["CACHE_TYPE"] = "null"
+CORS(app, origins=["http://127.0.0.1/:5000",
+     "http://127.0.0.1:5501"], supports_credentials=True)
+jwt = JWTManager(app)
+app.config['JWT_SECRET_KEY'] = 'abcdef'
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=15)
+app.config['JWT_TOKEN_LOCATION'] = ['cookies']
+app.config["JWT_COOKIE_CSRF_PROTECT"] = False
 
-@app.route("/") 
-def render_home() :
-    return render_template("index.html")
-@app.route("/config") 
-def render_configuration() : 
-    with open("config.txt") as f : 
-        return render_template("config.html" , config =  eval(f.read()) )
-@app.route("/setconfig",methods=["POST"])
-def setconfiguration() :
-      req = request.form
-      with open("config.txt") as f : 
-          config = eval(f.read())
-      for key , value in req.items() :
-           if type(config[key]) == int :
-               value = int(value)
-           config[key] = value 
-      with open("config.txt","w+") as f :
-          f.write(str(config))
-      return render_configuration()
-@app.route("/update")
-def update_chromedriver() :
-    shutil.copy( chromedriver_autoinstaller.install(path= "D:\\") , r'chromedriver.exe')
-    return "Finished Update"
+# your connection string
+client = MongoClient(
+    "mongodb+srv://venkatesh2004:venkatesh2004@cluster0.9x1ccpv.mongodb.net/?retryWrites=true&w=majority")
+db = client["demo"]
+users = db["users"]
+configs = db["config"]
 
-@app.route("/outstanding")
-def render_outstanding() : 
-    return render_template("outstanding.html")
-@app.route("/outstanding/<date>/<randomkey>") 
-def Outstanding(date,randomkey) : #date format in y-m-
-    if date == "today" :
-        date = datetime.now().strftime("%Y-%m-%d")
-    else : 
-        date = datetime.strptime(date,"%Y-%m-%d")
-    outstanding.main(date = date,days = 20) 
-    return send_file("outstandingreport.xlsx",as_attachment=True)
+# @app.route("/signup", methods=["POST"])
+# def register():
+#     new_user = request.get_json() # store the json body request
+#     new_user["password"] = hashlib.sha256(new_user["password"].encode("utf-8")).hexdigest() # encrpt password
+#     doc = users.find_one({"username": new_user["username"]}) # check if user exist
+#     if not doc:
+#         users.insert_one(new_user)
+#         return jsonify({'msg': 'User created successfully'}), 201
+#     else:
+#         return jsonify({'msg': 'Username already exists'}), 409
 
-@app.route("/creditlock")
-def Creditlock() : 
-    creditlock.main()
-    return {"status" : 1}
 
-@app.route('/eway')
-def render_eway():
-   beats=pd.read_excel('beat.xlsx')
-   beats=list(set(list(beats['Beat'])))
-   with open('eway\\vehicle.txt') as f : 
-    vehname=eval(f.read())
-   vehicle = {vehname[i] : i for i in vehname.keys()}
-   date = datetime.now().strftime("%Y-%m-%d")
-   return render_template('ewayindex.html',beats=beats,vehicle=vehicle,date=date)
-@app.route('/eway/generate',methods=['POST','GET'])
-def generateeway() :
-         x = request.form
-         data={}
-         for key in x.keys() :
-           if key not in ['vehicle','date1','date2','type'] :
-             data[key]=x[key]
-         types = x["type"]
-         date1=x['date1'].split('-')[2]+'/'+x['date1'].split('-')[1]+'/'+x['date1'].split('-')[0]
-         if x['date2']!='' :
-              date2=x['date2'].split('-')[2]+'/'+x['date2'].split('-')[1]+'/'+x['date2'].split('-')[0]
-         else :
-              date2=date1
-         response = EGenerate(types, date1 , date2 ,data )
-         if response is None :
-             return  redirect("/eway")
-         return send_file(response["fpath"],as_attachment=True)
 
-@app.route("/esync")
-def einv_sync() :
-    response = esync.sync()
-    return render_template("esync.html",response = response)
-webbrowser.open('http://127.0.0.1:1000/')
-app.run(threaded=True,debug=False,port=1000)
+
+@app.route("/login", methods=["GET"])
+def loginpage():
+    return app.send_static_file("login.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    login_details = request.get_json()  # store the json body request
+    # search for user in database
+    user_from_db = users.find_one({'username': login_details['username']})
+    if user_from_db:
+        encrpted_password = hashlib.sha256(
+            login_details['password'].encode("utf-8")).hexdigest()
+        if encrpted_password == user_from_db['password']:
+            access_token = create_access_token(
+                identity=user_from_db["username"])  # create jwt token
+            response = jsonify({"status" : True , "redirect" : "/"})
+            set_access_cookies(response, access_token)
+            return response
+        else:
+            return jsonify({"status": False ,"err": "Wrong Password"}), 401
+    else:
+        login_details["password"] = hashlib.sha256(
+            login_details['password'].encode("utf-8")).hexdigest()
+        users.insert_one(login_details)
+        response = jsonify({"status" : True , "redirect" : "/update"})
+        access_token = create_access_token(
+                identity= login_details["username"]) 
+        set_access_cookies(response, access_token)
+        return response
+
+
+@jwt.unauthorized_loader
+def custom_unauthorized_response(_err):
+    return redirect(url_for('login'))
+
+#Web page rendering :: start 
+@app.route("/", methods=["GET"])
+@jwt_required()
+def MainPage():
+    return app.send_static_file("index.html")
+
+@app.route("/update", methods=["GET"])
+@jwt_required()
+def UpdatePage():
+    return app.send_static_file("update.html")
+#Web page Rendering :: end 
+
+#Update Stuff ::: start 
+@app.route("/preload", methods=["GET"])
+@jwt_required()
+def Preload():
+    user = get_jwt_identity()
+    data = dict(users.find_one({"username" : user}))
+    required = ["ikea_user" , "ikea_pwd" , "dbName", "baseUrl" , "eway_user" , "eway_password" , 
+                "einv_user" , "einv_password" ]
+    data = { i : j for i , j in data.items() if i in required }
+    return jsonify(data)
+
+@app.route("/preDownload/<type>", methods=["GET"])
+@jwt_required()
+def PreDownload(type):
+    user = get_jwt_identity()
+    data = configs.find_one({"username" : user})
+    if type == "vehicle" : 
+        temp = pd.DataFrame([[i,j] for i , j in data["vehicles"].items()] , columns = ["Name","Vehicle No"])
+        return SendExcel( temp , "vehicles.xlsx")
+        
+
+@app.route("/verify", methods=["POST"])
+@jwt_required()
+def verify():
+    user = get_jwt_identity()
+    data = request.get_json()
+    [Type, data] = data
+    if Type == "ikea":
+        users.update_one({"username": user}, {"$set": data }, upsert=True)
+        status , err  = ikea(user,users,isReload=False).login() 
+        return jsonify({ "status" : status , "err" : err }) , 200 
+
+    if Type == "eway" or Type == "einvoice" :
+        users.update_one({"username": user}, {"$set": data }, upsert=True)
+        return jsonify({ "status" : True  , "err" : "Success" }) , 200 
+
+@app.route("/postUpdate", methods=["POST"])
+@jwt_required()
+def postUpdate():
+    user = get_jwt_identity()
+    data = dict(request.form)
+    users.update_one({"username" : user }, { "$set" : data }, upsert = True ) 
+    for  fname , file in dict(request.files).items() : 
+        if file : 
+           df = pd.read_excel(file)  
+           if fname == "vehicle" : 
+              update = { "vehicles" : { i : j  for [i,j] in df.values.tolist() }}
+              configs.update_one({ "username" : user} , { "$set" : update } , upsert =True )
+              return redirect("/")
+            #return jsonify({ "status" : True , "err" : "Success"})
+    
+#Update Ends :::
+
+
+
+#Eway and Einvoice :: Start 
+
+#Preloaders :: getbeats && getVehicle 
+@app.route("/getbeats", methods=["POST"])
+@jwt_required()
+def getBeats():
+    data = request.get_json()
+    user = get_jwt_identity()
+    session = ikea(user, users)
+    beats = session.getBeats(dateParser(
+        data["fromDate"]), dateParser(data["toDate"]))
+    return beats
+
+@app.route("/getvehicle", methods=["GET"])
+@jwt_required()
+def getVehicle():
+    user = get_jwt_identity()
+    vehicles = configs.find_one({"username": user})
+    if vehicles:
+        return vehicles["vehicles"]
+    else:
+        return jsonify({"err": "No vehciles exists"})
+#Preloaders :: End 
+
+#Login For Eway and Einvoice
+@app.route("/ewayLogin", methods=["POST", "GET"])
+@jwt_required()
+def ewayLogin():
+    user = get_jwt_identity()
+    types = request.args.get(
+        "types") if request.method == "GET" else request.get_json()["types"]
+    maps = {"einvoice": einvsite.Einvoice, "eway": ewaysite.Eway}
+    esession = maps[types]()
+    if request.method == "POST":
+        data = request.get_json()
+        data.update(json.loads(request.cookies.get("data")))
+        return jsonify(esession.login(user, users, data))
+    else:
+        img, data = esession.getCaptcha()
+        response = make_response(send_file(img, mimetype='image/aspx'))
+        response.set_cookie("data", json.dumps(data))
+        return response
+
+#Generate Eway or Einvoice , return the json ( which contains the excel )
+@app.route("/eGenerate", methods=["POST"])
+@jwt_required()
+def generateEway():
+    user = get_jwt_identity()
+    data = request.get_json()
+    data["fromDate"],data["toDate"] = dateParser(data["fromDate"]),dateParser(data["toDate"])
+    x = ikea(user,users).EGenerate(**data)
+    return x 
+    #if type(x) == dict :
+    #    return jsonify(x)
+    #return send_file( x ,  mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" , 
+    #                       download_name= data["types"] +  ".xlsx" )
+
+
+@app.route("/outstanding", methods=["POST"])
+@jwt_required()
+def Outstanding():
+    user = get_jwt_identity()
+    data = request.get_json()
+    date, days = dateParser(data["date"]), data["days"]
+    return ikea(user,users).outstanding(date , days)
+
+
+@app.route("/creditlock", methods=["GET"])
+@jwt_required()
+def CreditLock():
+    user = get_jwt_identity()
+    return ikea(user, users).creditlock(configs)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
